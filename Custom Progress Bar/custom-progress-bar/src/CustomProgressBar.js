69e2fcc5ab520a31e00da9147708ac91536a1b6e
mohanapriya-narapa <mohanapriya.narapa@outlook.com>
2024-11-10 14:17:24 -0600

    progress bar changes

--- Custom Progress Bar/custom-progress-bar/src/CustomProgressBar.js
+++ Custom Progress Bar/custom-progress-bar/src/CustomProgressBar.js
@@ -1,18 +1,23 @@
 import React, {useState, useEffect} from 'react'
 import './ProgressBar.css'
 function CustomProgressBar() {
-    const [startTransition, setTransition]=useState(false);
-    useEffect(()=>{
-const timer=setTimeout(()=>{
-    setTransition(true);
-}
-,10)
+    // const [startTransition, setTransition]=useState(false);
+//     useEffect(()=>{
+// const timer=setTimeout(()=>{
+//     setTransition(true);
+// }
+// ,10)
+const [progress, setProgress]=useState(0)
+useEffect(()=>{
+const interval= setInterval(()=>{
+  setProgress((prev)=>prev<100?prev+10:100)
+},1000)
+return(()=>clearInterval(interval))
+},[])
 
-return(()=>{clearTimeout(timer)});
-    },[])
   return (
     <div className='bar'>
- <div className={`bar-contents ${startTransition?'bar-contents-filled':''}`}></div>
+ <div className='bar-contents' style={{width:`${progress}%`}}></div>
     </div>
   );
 }
